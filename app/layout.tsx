3f67c113e4129745aeffb65098b5d8d50d32ab22
bleekerstudios <sofi@bleecker-studios.com>
2024-01-19 11:08:24 -0500

    new logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import { Analytics } from '@vercel/analytics/react';
 import Head from 'next/head';
 import Image from "next/image";
-import BleeckerLogo from "bleeckerlogo.svg"
+ 
 
 
 
@@ -61,12 +61,18 @@ export default function RootLayout({
         <title>Bleecker Studios</title>
         <meta property="og:title" content="Bleecker Studios" />
         <meta property="og:description" content="Everyone Has Ideas, We Bring Them To Life." />
-        <meta property="og:image" content="/bleeckerlogo.svg"/>
         <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/bleeckerlogo.svg" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/iphoneLogo.png" />
         <meta name="apple-mobile-web-app-title" content="Bleecker Studios" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+
+
+        <meta property="og:url" content="https://www.bleecker-studios.com" />
+  <meta property="og:type" content="website" />
+  <meta property="og:title" content="Bleecker Studios" />
+  <meta property="og:description" content="Everyone Has Ideas, We Bring Them To Life." />
+  <meta property="og:image" content="/iphoneLogo.png" />
 
 
 
