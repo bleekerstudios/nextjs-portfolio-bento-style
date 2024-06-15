e13e93fbcadc387c7b0118977fabafbe8b6c7f0d
bleekerstudios <sofi@bleecker-studios.com>
2024-06-15 10:59:03 -0400

    xx

--- config/site-config.ts
+++ config/site-config.ts
@@ -212,7 +212,7 @@ const GridItems: GridItemInterface[] = [
     layout: "2x4",
     type: "equipment",
     title: "Blog  ",
-    image: "/greylogo.svg",
+    image: "/greyfinal22.svg",
     equipments: [
       {
         title: "Certifications, Partnerships, & Technology",
